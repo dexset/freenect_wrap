6e9ab8b835324bd0e5a1b68372c1f30475404cd5
Akzhigitov Anton <akzhigitov.anton@yandex.ru>
2014-06-25 18:07:20 +0400

    fix

--- import/dwrap/freenect.d
+++ import/dwrap/freenect.d
@@ -1,12 +1,12 @@
 module dwrap.freenect;
 
-extern "C"
+extern (C)
 {
     uint initializeFreenectDevices();
     void deinitializeFreenectDevices();
     void processFreenectDevices();
-    bool getRGB( unsigned int devID, ubyte* buffer ); //memory must be allocated
-    bool getDepth( unsigned int devID, ushort* buffer ); //memory must be allocated
+    bool getRGB( uint, ubyte* ); //memory must be allocated
+    bool getDepth( uint, ushort* ); //memory must be allocated
 
     uint getRGBWidth();
     uint getRGBHeight();
